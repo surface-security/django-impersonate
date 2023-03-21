fe37b34973b120fd09dde3eee503052fd810a313
Filipe Pina <fopina@gmail.com>
2023-03-21 01:47:16 +0000

    x

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TEMP
 
-middleware needs to come *AFTER* `django.contrib.sessions.middleware.SessionMiddleware`
+middleware needs to come *AFTER* `django.contrib.auth.middleware.AuthenticationMiddleware`
+
+package could be named `django-admin-impersonate` instead but it actually works for any django app using `django.contrib.auth`, not only django-admin
+
+Missing a non-django-admin-specific utility to set the cookie
--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -44,10 +44,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'impersonate.middleware.ImpersonateMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
